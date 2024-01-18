cc0a0fc36732435f36caf52a47f74c37d86a3771
Alexis Lenogue <alexis.lenogue@epitech.eu>
2024-01-18 18:12:34 +0100

    app.js upgrade

--- Odyssey/App.js
+++ Odyssey/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import PreLogin from './pages/log/prelogin';
+import Login from './pages/log/login';
+import Accueil from './pages/accueil';
+import Profil from './pages/profil';
+import Ex1 from './pages/exercices/exercice_1';
+import Ex2 from './pages/exercices/exercice_2';
+import Ex3 from './pages/exercices/exercice_3';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="PRELOGIN">
+          <Stack.Screen name="PRELOGIN" component={PreLogin} />
+          <Stack.Screen name="LOGIN" component={Login} />
+          <Stack.Screen name="ACCUEIL" component={Accueil} />
+          <Stack.Screen name="PROFIL" component={Profil} />
+          <Stack.Screen name="EXE1" component={Ex1} />
+          <Stack.Screen name="EXE2" component={Ex2} />
+          <Stack.Screen name="EXE3" component={Ex3} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
